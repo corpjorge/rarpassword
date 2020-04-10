4615a157e191a6f4441f4d0c276877f8b821bffc
Jorge Eduardo <corpjorge@hotmail.com>
2020-04-09 22:11:14 -0500

    Create rarPassword.php

---
+++ src/rarPassword.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace CorpJorge\rarpassword;
+
+
+class rarPassword
+{
+    public function hasTomatoes($bool = true)
+    {
+        error_reporting(0);
+
+        $filepath = "C:/ruta/archivo.rar";
+
+        $inicio = array(
+            'inicio1',
+            'Inicio2'
+        );
+
+        $final = array(
+            'final1',
+            'final2'
+        );
+
+        foreach ($inicio as $ini ) {
+
+            foreach ($final as $fin) {
+                $pass = $ini.'.'.$fin;
+
+                $rar_file = rar_open($filepath,$pass);
+                $list = rar_list($rar_file);
+
+                if ($list) {
+                    echo $pass;
+                }
+                rar_close($rar_file);
+            }
+        }
+    }
+
+}
